3e29e0dd1c368cf4323209e5de15548241071f80
Mike Lee <109776429+MikeLeeDeveloper@users.noreply.github.com>
2022-08-06 15:53:31 -0700

    UpdateVariableNames

--- Python/PlayYoutubeVideo/Selenium/Commands.py
+++ Python/PlayYoutubeVideo/Selenium/Commands.py
@@ -3,27 +3,27 @@ import time, datetime
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as Ec
 
 #Global Variables
-localDirectory = os.getcwd()
+currentDirectory = os.getcwd()
 
 def launchChrome(url):
 	#Navigate to URL
-	browser = webdriver.Chrome(localDirectory + "\Selenium\chromedriver.exe")
+	browser = webdriver.Chrome(currentDirectory + "\Selenium\chromedriver.exe")
 	#browser.maximize_window()
 	browser.get(url)
 
 	#Wait for play button to load, then click
-	WebDriverWait(browser, 15).until(EC.element_to_be_clickable(
+	WebDriverWait(browser, 15).until(Ec.element_to_be_clickable(
     (By.XPATH, "//button[@aria-label='Play']"))).click()
 
 	#Get video length
-	getDuration = browser.find_element(By.CLASS_NAME,"ytp-time-duration").text
-	durationToTime = time.strptime(getDuration, '%M:%S')
-	durationToSeconds = datetime.timedelta(minutes=durationToTime.tm_min, 
+	durationToTime = time.strptime(browser.find_element(
+		By.CLASS_NAME,"ytp-time-duration").text, '%M:%S')
+	videoLength = datetime.timedelta(minutes=durationToTime.tm_min, 
 		seconds=durationToTime.tm_sec).total_seconds()
 
     #Persist browser until video ends
-	time.sleep(durationToSeconds + 5)
+	time.sleep(videoLength + 5)
 	browser.quit()
