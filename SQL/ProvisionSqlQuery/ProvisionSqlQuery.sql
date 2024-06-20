ee3f09609988d7b58425f3fb688f4639b7a9c8e9
Mike Lee <109776429+MikeLeeDeveloper@users.noreply.github.com>
2024-06-20 13:56:32 -0700

    Update ProvisionSqlQuery.sql

--- SQL/ProvisionSqlQuery/ProvisionSqlQuery.sql
+++ SQL/ProvisionSqlQuery/ProvisionSqlQuery.sql
@@ -13,8 +13,8 @@ DECLARE @MaxFieldLength INT =
 
 -- Get Column Names
 SELECT [ColumnName] =			c.[name]
-      ,[DataType] =				t.[Name]
-      ,[MaxLength] =			c.[max_length]
+	  ,[DataType] =				t.[Name]
+	  ,[MaxLength] =			c.[max_length]
 	  ,[Precision] =			c.[precision]
 	  ,[Scale] =				c.[scale]
 	  ,[SortOrder] =			c.[column_id]
@@ -29,14 +29,37 @@ SELECT [ColumnName] =			c.[name]
 									END
 									-- Comment line below if no null values
 									+ ' = NULL'
-	  ,[InsertStatement] =		',[' + c.[name] + '] = @' + REPLACE(c.[Name],'+','_')
-	  -- 
+	  ,[InsertStatement] =		'  ,[' + c.[name] + '] = @' + REPLACE(c.[Name],'+','_')
 	  ,[MergeUpdateStatement] = ',' + @MergeTarget + '.[' + c.[name] + '] = ' + @MergeSource+ '.[' + c.[name] + ']'
+	  ,[SqlParameter] =			'    new SqlParameter(@"' + REPLACE(c.[Name],'+','_') + '",SqlDbType.' +
+									CASE
+										WHEN t.[Name] = 'int'
+											THEN 'Int'
+										WHEN t.[Name] = 'tinyint'
+											THEN 'TinyInt'
+										WHEN t.[Name] = 'decimal'
+											THEN 'Decimal,' + CONVERT(VARCHAR(MAX),c.[precision])
+										WHEN t.[Name] = 'bit'
+											THEN 'Bit'
+										WHEN t.[Name] = 'datetime'
+											THEN 'DateTime'
+										WHEN t.[Name] = 'char'
+											THEN 'Char,' + CONVERT(VARCHAR(MAX),c.[max_length])
+										WHEN t.[Name] = 'varchar'
+											THEN 'VarChar,' + CONVERT(VARCHAR(MAX),c.[max_length])
+										WHEN t.[Name] = 'nvarchar'
+											THEN 'NVarChar,' + CONVERT(VARCHAR(MAX),c.[max_length])
+										ELSE '' 
+									END + '),'
+		,[SqlValue] =			'sqlparam[' + CONVERT(VARCHAR(MAX), ROW_NUMBER() OVER(ORDER BY c.[column_id]) - 1) 
+							  + '].Value =  // ' + c.[name]
 FROM sys.columns as c
 INNER JOIN sys.types as t ON c.[user_type_id] = t.[user_type_id]
 WHERE c.[object_id] = OBJECT_ID(@TableName)
--- Uncomment line below to exclude identity column
---AND c.[is_identity] != 1
+-- Comment line below to include identity column
+AND c.[is_identity] != 1
+ORDER BY c.[column_id] ASC
+--ORDER BY c.[name] ASC
 ORDER BY c.[name] ASC
 -- Uncomment line below to order by table order
 --ORDER BY c.[column_id] ASC
